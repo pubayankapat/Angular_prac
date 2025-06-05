0fee1625f4fc154c1cf40da626748cd4e58f146d
pubayankapat <pubayankapat@gmail.com>
2025-06-05 17:36:23 +0530

    Updating files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
+import { Signup } from "./signup/signup";
 
 @Component({
   selector: 'app-root',
-  imports: [Login],
+  imports: [Login, Signup],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

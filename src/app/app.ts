0e8fba5931caf99bb3c4a5a899a854866c4f7f6a
pubayankapat <pubayankapat@gmail.com>
2025-06-06 08:53:57 +0530

    Removing component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './login/login';
-import { Signup } from "./signup/signup";
+
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Signup],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

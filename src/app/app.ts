36def0d7553c29bf0fac726d8a6620dd5640fbad
pubayankapat <pubayankapat@gmail.com>
2025-06-09 09:11:06 +0530

    Updating file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
